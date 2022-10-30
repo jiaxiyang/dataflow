862d4fe0b7be8327ff6b4e4218097fabfbcd0eab
xiyang.jia <xiyang.jia@phigent.ai>
2022-10-30 20:08:12 +0800

    [feat](init): init and add 3rd-party libs

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # dataflow
+
+## 目标
+
+编写一个基于 dag 的任务调度框架, 主要有两大部分组成，一部分是任务依赖关系表示的图，一部分是图的运行环境, 第一版先基于 boost::graph 进行图的表示。
+
+## TODO
+
+### graph
+
+    - [ ] graph
+    - [ ] subgraph
+    - [ ] node
+    - [ ] messae
+
+### runtime
+
+    - [ ] scheduler
+    - [ ] executor
+
+### basic tools
+
+    - [ ] queue
+    - [ ] threadpool
+    - [ ] memorypool
+
+### 3rd-party
+
+    - [ ] doctest
+    - [ ] nlohmann/json
+    - [ ] spdlog
+
+## References
+
+1. [taskflow](https://github.com/taskflow/taskflow)
+1. [workflow](https://github.com/sogou/workflow)
+1. [CGraph](https://github.com/ChunelFeng/CGraph)
+1. [gparallel](https://github.com/galois-advertising/gparallel)
---
+++ scripts/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BUILD_TYPE=Release
+BUILD_DIR=build
+INSTALL_DIR=${BUILD_DIR}/$(basename "$PWD")_$(echo ${BUILD_TYPE} | tr '[:upper:]' '[:lower:]')
+
+BUILD_SYSTEM=Ninja
+if ! [ -x "$(command -v ninja)" ]; then
+	BUILD_SYSTEM='Unix Makefiles'
+fi
+
+rm -rf ${BUILD_DIR}
+
+cmake -G "${BUILD_SYSTEM}" -S . -B ${BUILD_DIR} \
+	-DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+	-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+	-DCMAKE_VERBOSE_MAKEFILE=OFF
+# -DCMAKE_C_COMPILER=clang \
+# -DCMAKE_CXX_COMPILER=clang++
+
+cmake --build ${BUILD_DIR}
+# cmake --build ${BUILD_DIR} --target ${TARGET_NAME}
+
+#(cd ${BUILD_DIR} && make test)
+# ctest --test-dir ${BUILD_DIR} # after cmake 3.20
+
+cmake --install ${BUILD_DIR} --strip --prefix ${INSTALL_DIR}
+
+# mv ${BUILD_DIR}/compile_commands.json .
+#clang-tidy *.cpp
+
+# cpack --config ${BUILD_DIR}/CPackConfig.cmake
+./build/examples/json_example
+./build/examples/spdlog_example
